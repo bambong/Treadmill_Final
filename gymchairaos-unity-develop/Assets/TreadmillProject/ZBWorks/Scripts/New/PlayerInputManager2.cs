8f6ee0310cac1326e46d07d5dae6c9678e8124ad
gb0520 <mcpcgb@gmail.com>
2023-09-12 02:22:00 +0900

    장애물게임 좌우이동 로직변경

--- gymchairaos-unity-develop/Assets/TreadmillProject/ZBWorks/Scripts/New/PlayerInputManager2.cs
+++ gymchairaos-unity-develop/Assets/TreadmillProject/ZBWorks/Scripts/New/PlayerInputManager2.cs
@@ -8,13 +8,10 @@ namespace ZB
 {
     public class PlayerInputManager2 : MonoBehaviour
     {
-        [SerializeField] Transform playerBody;
+        [SerializeField] Transform tf;
 
         [SerializeField] bool leftReceived;
         [SerializeField] bool rightReceived;
-        [SerializeField] float leftPos;
-        [SerializeField] float rightPos;
-        [SerializeField] float sideMoveDuration;
 
         [SerializeField] float leftRpm;
         [SerializeField] float rightRpm;
@@ -22,13 +19,39 @@ namespace ZB
 
         [SerializeField] bool checking;
         [SerializeField] List<ParticleSystem> wheelEffects;
+
+        [Space]
+        [Header("이동관련")]
+        [SerializeField] float minInterval; //좌우 이동이 일어나는 최소간격
+        [SerializeField] float moveMultiple;
+        [SerializeField] float maxMove;
+        [SerializeField] float move;
+        float intervalAbs { get => Mathf.Abs(rightRpm - leftRpm); }
+
+        [Space]
+        [SerializeField] float outPos_left;
+        [SerializeField] float outPos_right;
+
+        [Space]
+        [Header("회전관련")]
+        [SerializeField] float rotMultiple;
+        float currentRotTarget;
+
+        TokenInputManager token { get => TokenInputManager.Instance; }
+
         public void ResetState()
         {
-            playerBody.transform.DOMoveX(-0.6f, 1).SetEase(Ease.OutQuart).SetDelay(1.5f);
+            tf.rotation = Quaternion.Euler(0, 0, 0);
+
+            tf.DOKill();
+            tf.DOMoveX(-0.6f, 1).SetEase(Ease.OutQuart).SetDelay(1.5f);
             leftReceived = true;
             rightReceived = false;
             TokenInputManager.Instance.Save_left_rpm = 0;
             TokenInputManager.Instance.Save_right_rpm = 0;
+
+            currentRotTarget = 0;
+            move = 0;
         }
         public void AddLeftToken()
         {
@@ -69,7 +92,7 @@ namespace ZB
 
         void Awake()
         {
-            resetPos = playerBody.position;
+            resetPos = tf.position;
 
 #if UNITY_EDITOR
             TokenInputManager.Instance.AddLeftTokenEvent(AddLeftToken);
@@ -83,47 +106,65 @@ namespace ZB
 #if UNITY_EDITOR
             SideMove();
 #endif
+
+            if (Input.GetKeyDown(KeyCode.R))
+            { token.Save_left_rpm = 0; token.Save_right_rpm = 0; }
         }
 
         void SideMove()
         {
             if (checking)
             {
-                leftRpm = TokenInputManager.Instance.Save_left_rpm;
-                rightRpm = TokenInputManager.Instance.Save_right_rpm;
-                if (leftRpm > 1 && leftRpm > rightRpm && !rightReceived)
-                {
-                    Debug.Log("RightMove");
-                    playerBody.DOKill();
-                    playerBody.DOMoveX(rightPos, sideMoveDuration).SetEase(Ease.InQuart);
-                    playerBody.DORotate(new Vector3(0, 15, 0), 0.75f).SetEase(Ease.OutQuart);
-                    playerBody.DORotate(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.InOutBack).SetDelay(0.75f);
+                leftRpm = token.Save_left_rpm;
+                rightRpm = token.Save_right_rpm;
 
-                    rightReceived = true;
-                    leftReceived = false;
+                //프레임마다 이동할 정도 구함
+                if (intervalAbs < minInterval)
+                    move = 0;
+                else
+                {
+                    move = (rightRpm - leftRpm) * moveMultiple;
+
+                    if (rightRpm > leftRpm &&
+                        move > maxMove)
+                        move = maxMove;
+                    else if (move <= -maxMove)
+                        move = -maxMove;
                 }
-                if (rightRpm > 1 && rightRpm > leftRpm && !leftReceived)
-                {
-                    Debug.Log("LEFTMOVE");
-                    playerBody.DOKill();
-                    playerBody.DOMoveX(leftPos, sideMoveDuration).SetEase(Ease.InQuart);
-                    playerBody.DORotate(new Vector3(0, -15, 0), 0.75f).SetEase(Ease.OutQuart);
-                    playerBody.DORotate(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.InOutBack).SetDelay(0.75f);
 
-                    leftReceived = true;
-                    rightReceived = false;
+                //이동
+                if (tf.position.x <= outPos_left && move < 0)
+                {
+                    tf.position = new Vector3(outPos_left, tf.position.y, tf.position.z);
+                }
+                else if (tf.position.x >= outPos_right && move > 0)
+                {
+                    tf.position = new Vector3(outPos_right, tf.position.y, tf.position.z);
+                }
+                else
+                {
+                    tf.position += new Vector3(move * Time.deltaTime, 0, 0);
+                }
+
+
+                //이동 정도에 따른 차체 회전
+                if (currentRotTarget != move * rotMultiple)
+                {
+                    currentRotTarget = move * rotMultiple;
+                    tf.DOKill();
+                    tf.DORotate(new Vector3(tf.eulerAngles.x, move * rotMultiple, tf.eulerAngles.z), 0.5f);
                 }
             }
         }
 
         public void MoveBack_OnDie()
         {
-            playerBody.DOMoveZ(-8, 1).SetEase(Ease.OutQuart);
+            tf.DOMoveZ(-8, 1).SetEase(Ease.OutQuart);
         }
         public void MoveFront_OnRebirth()
         {
-            playerBody.transform.position = new Vector3(resetPos.x , resetPos.y, -8);
-            playerBody.DOMove(resetPos, 1.6f).SetEase(Ease.OutQuart);
+            tf.position = resetPos + new Vector3(0, 0, -2.5f);
+            tf.DOMove(resetPos, 1.6f).SetEase(Ease.OutQuart);
         }
     }
 }
