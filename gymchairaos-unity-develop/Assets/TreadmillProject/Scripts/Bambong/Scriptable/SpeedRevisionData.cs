acb2691b017a1d12c468e6507c772eff4966ea4b
bambong <alsrbs2gh@gmail.com>
2023-09-25 00:49:22 +0900

    UDP 통신을 통해 센서에서 데이터를 받도록 수정

--- gymchairaos-unity-develop/Assets/TreadmillProject/Scripts/Bambong/Scriptable/SpeedRevisionData.cs
+++ gymchairaos-unity-develop/Assets/TreadmillProject/Scripts/Bambong/Scriptable/SpeedRevisionData.cs
@@ -8,11 +8,10 @@ namespace bambong
     [Serializable]
     public struct SpeedChangeData
     {
-        public float ratio;
-        public float time;
-        public float speed;
+        public float ratio; // 얼만큼의 거리에서 변화할건지 
+        public float time; // 변화하는데 까지 걸리는 시간
+        public float speed; // 어떤 속도로 변화 할건지
     }
-    
 
 
     [CreateAssetMenu(fileName = "SpeedRevisionData",menuName = "Scriptable/SpeedRevisionData",order = 0)]
@@ -20,15 +19,15 @@ namespace bambong
     {
         public List<SpeedChangeData> pairs;
 
-        public SpeedChangeData GetCollectAreaData(float key) 
+        public SpeedChangeData GetCollectAreaData(float ratio)  // 전체 레이스 중 얼만큼 왔는지 
         {
             foreach(var data in pairs) 
             {
-                if( data.ratio < key) 
+                if( data.ratio < ratio) 
                 {
                     continue;
                 }
-                return data;
+                return data;  // 범위에 알맞은 데이터를 반환
             }
             return pairs[pairs.Count - 1];
         }
