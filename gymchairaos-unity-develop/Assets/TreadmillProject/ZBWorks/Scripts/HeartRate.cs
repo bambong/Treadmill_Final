5058bbaae2262da62579f506dbb7ec015a87956a
gb0520 <mcpcgb@gmail.com>
2023-09-26 00:20:55 +0900

    BPM 나오도록 적용

--- gymchairaos-unity-develop/Assets/TreadmillProject/ZBWorks/Scripts/HeartRate.cs
+++ gymchairaos-unity-develop/Assets/TreadmillProject/ZBWorks/Scripts/HeartRate.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using bambong;
+using TMPro;
 
 public class HeartRate : MonoBehaviour
 {
-
+    [SerializeField] TextMeshProUGUI tmpro;
+    void Update()
+    {
+        tmpro.text = Managers.Token.Bpm.ToString();
+    }
 }
